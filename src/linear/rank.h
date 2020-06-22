d3a5e643722a7500af238b5f42d318ea27f4b89a
moddyz <moddy3d@gmail.com>
2020-06-22 23:51:52 +1200

    Added MaxRank helper function.

---
+++ src/linear/rank.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/// \file rank.h
+///
+/// Matrix rank.
+///
+/// The \em rank of a matrix is the number of pivot columns it possesses, or in other words,
+/// the number of independent columns.
+
+#include <linear/linear.h>
+
+LINEAR_ALGEBRA_NS_OPEN
+
+/// Compute the \em maximum rank a matrix of type \p MatrixT can have.
+///
+/// \return the maximum rank.
+template< typename MatrixT >
+constexpr inline size_t MaxRank()
+{
+    return std::min( MatrixT::RowCount(), MatrixT::ColumnCount() );
+}
+
+LINEAR_ALGEBRA_NS_CLOSE
